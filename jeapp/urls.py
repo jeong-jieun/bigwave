452ebec24e864b0460008029c3ac727d40acd278
지은 <wjdwldms66@naver.com>
2023-10-16 14:07:43 +0900

    je commit

--- jeapp/urls.py
+++ jeapp/urls.py
@@ -3,13 +3,15 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    
-    ### 부가서비스페이지 (http://127.0.0.1:8000/je/schedule/cafe)
-    path('schedule/cafe/', views.cafe),
-    ### 부가서비스페이지 (http://127.0.0.1:8000/je/schedule)
+   
+    ### 카페 부가서비스페이지 (http://127.0.0.1:8000/je/service/cafe)
+    path('service/다대선착장/', views.dadae),
+    ### 카페 부가서비스페이지 (http://127.0.0.1:8000/je/service/cafe)
+    path('service/cafe/', views.cafe),
+    ### 스케줄 조회 페이지 (http://127.0.0.1:8000/je/schedule)
     path('schedule/', views.schedule),
-    ### 결제페이지 (http://127.0.0.1:8000/je/booking)
-    path('booking/', views.setbookingInsert),
+    ### 결제버튼 클릭시 db저장 (http://127.0.0.1:8000/je/save_reservation)
+    path('save_reservation/', views.save_reservation, name='save_reservation'),
     ### 부가서비스페이지 (http://127.0.0.1:8000/je/service)
     path('service/', views.service, name='search_action'),
     ### 로그인페이지 (http://127.0.0.1:8000/je/login)
